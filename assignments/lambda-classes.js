9e4109ac45616fcc1a32a557d319cae703d0ac0c
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2019-10-17 17:05:08 -0400

    [Fix] Student listSubject method changed to .forEach from .map

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -7,7 +7,6 @@ class Person{
     }
 
     speak(){
-        console.log(`Hello, my name is ${this.name} and I am from ${this.location}.`);
         return `Hello, my name is ${this.name} and I am from ${this.location}.`;
     }
 }
@@ -21,12 +20,10 @@ class Instructor extends Person{
     }
 
     demo(subject){
-        console.log(`Today we are learning about ${subject}`);
-        return `Today we are learning about ${subject}`;
+        return `Today we are learning about ${subject}.`;
     }
 
     grade(student, subject){
-        console.log(`${student.name} receives a perfect score on ${subject}`);
         return `${student.name} receives a perfect score on ${subject}`;
     }
 }
@@ -40,17 +37,17 @@ class Student extends Person{
     }
 
     listSubjects(){
-        this.favSubjects.map(subject => {console.log(`This is one of my favorite subjects: ${subject}`)});
-        return this.favSubjects.map(subject => `This is one of my favorite subjects: ${subject}`);
+        return this.favSubjects.forEach(subject => `This is one of my favorite subjects: ${subject}`);
     }
     
     prAssignement(subject){
-        console.log(`${this.name} has submitted a PR for ${subject}`);
+   
         return `${this.name} has submitted a PR for ${subject}`;
     }
 
     sprintChallenge(subject){
-        console.log(`${this.name} has begun sprint chanllenge on ${subject}`);
+        //console.log(`${this.name} has begun sprint chanllenge on ${subject}`);
+        return `${this.name} has begun sprint chanllenge on ${subject}`;
     }
 }
 
@@ -72,3 +69,46 @@ class ProjectManager extends Instructor{
         return `${this.name} debugs ${student.name}'s code on ${subject}.`;
     }
 }
+
+//Instances
+
+const person1 = new Person({
+    name: 'Corey',
+    age: 42,
+    location: 'NYC'
+})
+
+
+const instructor1 = new Instructor({
+    name: 'John Smith',
+    age: '32',
+    location: 'Silicon Valley',
+    specialty: 'OOP',
+    favLanguage: 'Python',
+    catchPhrase: 'Yippy Kai Yay....'
+})
+
+
+ const student1 = new Student({
+     name: "Mike",
+     age: '23',
+     location: 'Florida',
+     previousBackground: 'unemployed',
+     className: 'Web25',
+     favSubjects: ['HTML', 'CSS', 'JS', 'React']
+ })
+
+
+//Conle.log
+//Person
+console.log(person1.name, person1.age, person1.location);
+console.log(person1.speak());
+
+//Instructor
+console.log(instructor1);
+console.log(instructor1.demo('data structures'));
+
+//Student
+console.log(student1);
+console.log(student1.listSubjects());
+console.log(student1.prAssignement(student1.favSubjects[2]));
