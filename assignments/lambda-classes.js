e89cd907822c86bf1f9c5c1c34a1a86fa48216f6
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2019-10-17 15:58:31 -0400

    [Add] Created Person class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,8 @@
 // CODE here for your Lambda Classes
+class Person{
+    constructor(personAttrs){
+        this.name = personAttrs.name,
+        this.age = personAttrs.age,
+        this.location = personAttrs.location
+    }
+}
