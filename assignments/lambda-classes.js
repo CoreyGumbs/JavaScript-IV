098dbbaa875334403b0552875a16a6af8dacebfc
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2019-10-17 16:05:52 -0400

    [Add] Created Instructor class with properties and methods

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -11,3 +11,22 @@ class Person{
         return `Hello, my name is ${this.name} and I am from ${this.location}.`;
     }
 }
+
+class Instructor extends Person{
+    constructor(instructorAttrs){
+        super(instructorAttrs);
+        this.specialty = instructorAttrs.specialty,
+        this.favLanguage = instructorAttrs.favLanguage,
+        this.catchPhrase = instructorAttrs.catchPhrase
+    }
+
+    demo(subject){
+        console.log(`Today we are learning about ${subject}`);
+        return `Today we are learning about ${subject}`;
+    }
+
+    grade(student, subject){
+        console.log(`${student.name} receives a perfect score on ${subject}`);
+        return `${student.name} receives a perfect score on ${subject}`;
+    }
+}
